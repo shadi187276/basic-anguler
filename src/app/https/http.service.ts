c97ec04856a494dba1451913dd979025278a954e
shahid <sm187276@gmail.com>
2024-09-11 21:08:20 +0500

    play with production

--- src/app/https/http.service.ts
+++ src/app/https/http.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../interfaces/product';
-import { environment } from 'src/envirment/environment';
+import { environment } from 'src/envirments/environment';
 
 @Injectable({
   providedIn: 'root',
