77c5f4788a5f8e27ec619cc8db88f4d3d7216771
shahid <sm187276@gmail.com>
2024-09-26 19:11:12 +0500

    test 8

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,4 +1,4 @@
-const { app } = require('dist/crudApp/server/main');
+const { app } = require('../../dist/crudApp/server/main');
 const serverless = require('serverless-http');
 
 exports.handler = serverless(app);
