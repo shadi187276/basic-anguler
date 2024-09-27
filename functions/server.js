b389d463f376a5b3651f8d5605e902a55734630e
shahid <sm187276@gmail.com>
2024-09-27 14:32:14 +0500

    test12

---
+++ functions/server.js
@@ -0,0 +1,14 @@
+const server = require('../dist/crudApp/server/main');
+const express = require('express');
+const serverless = require('serverless-http'); 
+const app = express();
+const path = require('path'); // Change here
+
+// Serve static files from /browser
+app.use(express.static(path.join(__dirname, '../dist/crudApp/browser'))); // Update to use path.join
+
+// Use the existing server logic for SSR
+app.use((req, res) => server.app(req, res));
+
+// Export handler for Netlify
+exports.handler = serverless(app);
