c251602d414bc647b8f3899a34983f758f415692
shahid <sm187276@gmail.com>
2024-09-27 15:23:31 +0500

    test14

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -5,7 +5,7 @@ const app = express();
 const path = require('path'); // Change here
 
 // Serve static files from /browser
-app.use(express.static(path.join(__dirname, '../dist/crudApp/browser'))); // Update to use path.join
+app.use(express.static(path.join(__dirname, '../../dist/crudApp/browser'))); // Update to use path.join
 
 // Use the existing server logic for SSR
 app.use((req, res) => server.app(req, res));
