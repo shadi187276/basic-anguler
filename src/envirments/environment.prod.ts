86aaf3e0c870cd68021977e6dd052aaea3c1fea3
shahid <sm187276@gmail.com>
2024-09-27 20:24:23 +0500

    test15

--- src/envirments/environment.prod.ts
+++ src/envirments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://basic-nestjs-git-main-shahid-mehmoods-projects.vercel.app',  // API URL for production
+    // apiUrl: 'https://basic-nestjs-git-main-shahid-mehmoods-projects.vercel.app',  // API URL for production
+    apiUrl: 'http://localhost:3000',  // API URL for production
+
   };
